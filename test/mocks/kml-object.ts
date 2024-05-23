633572e896501ba4fd703110e55622735675b35f
ItaloCobains <italobrandao.contato@gmail.com>
2024-05-23 19:14:33 -0300

    implement index.ts to export functions

--- test/mocks/kml-object.ts
+++ test/mocks/kml-object.ts
@@ -1,19 +1,19 @@
-export const kmlObject = {
-    kml: {
-      Document: [
-        {
-          name: ["Sample KML"],
-          Placemark: [
-            {
-              name: ["Sample Location"],
-              Point: [
-                {
-                  coordinates: ["-122.0822035425683,37.42228990140251,0"]
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    }
-  };
+import { KMLObject } from '../../src/interfaces';
+
+export const kmlObject: KMLObject = {
+  kml: {
+    Document: [
+      {
+        name: 'Sample KML',
+        Placemark: [
+          {
+            name: 'Sample Location',
+            Point: {
+              coordinates: ['-122.0822035425683,37.42228990140251,0'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
